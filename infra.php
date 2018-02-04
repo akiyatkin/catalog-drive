0fcf38ce2d0c6c879b9f3739e9502efa60dcfae1
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-02-04 19:14:12 +0400

    fix parent

--- infra.php
+++ infra.php
@@ -6,7 +6,20 @@ use infrajs\catalog\Catalog;
 
 Event::handler('Catalog.oninit', function(&$data){
 	$options = Catalog::getOptions();
+
+	
 	$data2 = Sheets::init('1Q34TcZvy-MexZPJS8gl3GYo8dK-6l5uU', $options);
+	
 	$data = Xlsx::merge([$data,$data2]);
 	
+	Xlsx::runGroups($data, function &(&$group) {
+		$r = null;
+		unset($group['parent']);
+		return $r;
+	});
+	Xlsx::runPoss($data, function &(&$pos) {
+		$r = null;
+		unset($pos['parent']);
+		return $r;
+	});
 });
