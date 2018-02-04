8d354879b00ea054b5e3956342a2137602e300d6
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-02-05 02:32:44 +0400

    fix

--- infra.php
+++ infra.php
@@ -6,10 +6,8 @@ use infrajs\catalog\Catalog;
 
 Event::handler('Catalog.oninit', function(&$data){
 	$options = Catalog::getOptions();
-
-	
 	$data2 = Sheets::init('1Q34TcZvy-MexZPJS8gl3GYo8dK-6l5uU', $options);
-	
+
 	$data = Xlsx::merge([$data,$data2]);
 	
 	Xlsx::runGroups($data, function &(&$group) {
