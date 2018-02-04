7a2f983a73e804d4b757de997af912215a8a17d1
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-02-04 13:43:15 +0400

    fix psr4

--- infra.php
+++ infra.php
@@ -3,8 +3,6 @@ use infrajs\event\Event;
 use akiyatkin\catalog\drive\Sheets;
 use infrajs\excel\Xlsx;
 
-require_once('index/catalog-drive/Sheets.php');
-
 Event::handler('Catalog.oninit', function(&$data){
 
 	$data2 = Sheets::init('1Q34TcZvy-MexZPJS8gl3GYo8dK-6l5uU');
