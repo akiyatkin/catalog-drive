c446292c45d855f59fc35f68d0e9fdae2ca77451
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-03-23 15:00:24 +0400

    fix

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 use infrajs\rest\Rest;
 use infrajs\ans\Ans;
 use infrajs\access\Access;
+use infrajs\nostore\Nostore;
 use akiyatkin\catalog\drive\Sheets;
 
+Access::test(true);
 $res = Rest::get(function( $id = false ){
 	if (!$id) $id = Sheets::$conf['folder'];
 	return Sheets::init($id);
